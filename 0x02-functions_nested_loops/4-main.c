79af36ae21ddb23685c0793905a7a1419292c4b5
ynwvroy <bennetfrmdao@gmail.com>
2023-03-21 07:02:01 -0700

    My first commit

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -9,13 +9,13 @@ int main(void)
 {
     int r;
 
-    r = _isalpha('H');
+    r = _isalpha ('H');
     _putchar(r + '0');
-    r = _isalpha('o');
+    r = _isalpha ('o');
     _putchar(r + '0');
-    r = _isalpha(108);
+    r = _isalpha (108);
     _putchar(r + '0');
-    r = _isalpha(';');
+    r = _isalpha (';');
     _putchar(r + '0');
     _putchar('\n');
     return (0);
