47f0129e3c5a914368cf7e5a53aff5c516c4fbc4
ynwvroy <bennetfrmdao@gmail.com>
2023-04-03 08:54:02 -0700

    My first commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _memset - Entry point
- * @s: pointed destination
- * @b: constant byte
- * @n: bytes
+ * _memset - Input point
+ * @s: pointing the destination
+ * @b: the constant byte
+ * @n: the bytes
  * Return: Always 0 (Success)
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int x;
 
-	for (i = 0; i < n; i++)
-		s[i] = b;
+	for (x = 0; x < n; x++)
+		s[x] = b;
 	return (s);
 }
