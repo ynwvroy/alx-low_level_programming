950ffad5175f3a379031dc9aef190a4f2c29edff
ynwvroy <bennetfrmdao@gmail.com>
2023-04-03 07:40:53 -0700

    My first commit

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -15,7 +15,7 @@ char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
 
-void print_chessboard(char (*a)[0]);
+void print_chessboard(char (*a)[8]);
 
 void print_diagsums(int *a, int size);
 
