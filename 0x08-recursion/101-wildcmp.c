d31ceec1f8194dfe45eb61447068276cca9017e5
ynwvroy <bennetfrmdao@gmail.com>
2023-04-05 11:24:33 -0700

    Inception is not impossible

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings
+ * wildcmp - comparison of the two strings
  * @s1: string 1.
- * @s2: string 2. It can contain a * as a special character.
- * Return: 1 if are identical, 0 if not.
+ * @s2: string 2. Containing a * as a special character.
+ * Return: 1 if found to be identical, 0 if not.
  */
 
 int wildcmp(char *s1, char *s2)
