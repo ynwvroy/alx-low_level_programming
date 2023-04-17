48d2690d623f1a51d4fd415ff7ce8d86029bf2ce
ynwvroy <bennetfrmdao@gmail.com>
2023-04-17 07:14:22 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,12 +2,12 @@
 #define _HEADER_
 
 /**
- * struct dog - The dog attribution
- * @name: Title of the dog
- * @age: Age of the dog
- * @owner: The dog handler
+ * struct dog - Dog attributes
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
  *
- * Description: Attributes of a dog
+ * Description: The attributes of a dog
  */
 
 struct dog
@@ -18,9 +18,8 @@ struct dog
 };
 
 /**
- * dog_t - Typedef for the dog structure
+ * dog_t - Typedef for dog structure
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
