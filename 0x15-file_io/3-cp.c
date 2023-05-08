76892a785e73897686a28836dfb14880e2dfc541
ynwvroy <bennetfrmdao@gmail.com>
2023-05-08 15:23:50 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,17 +13,17 @@ void close_file(int f1);
 
 char *create_buffer(char *file)
 {
-	char *b;
+	char *buffer;
 
-	b = malloc(sizeof(char) * 1024);
+	buffer = malloc(sizeof(char) * 1024);
 
-	if (b == NULL)
+	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", file);
 		exit(99);
 	}
-	return (b);
+	return (buffer);
 }
 
 /**
@@ -40,7 +40,7 @@ void close_file(int f1)
 	if (v == -1)
 	{
 		dprintf(STDERR_FILENO,
-				"Error: Can't close f1 %d\n", f1);
+				"Erro: Can't close f1 %d\n", f1);
 		exit(100);
 	}
 }
@@ -54,46 +54,47 @@ void close_file(int f1)
 
 int main(int argc, char *argv[])
 {
-	int f;
-	int t;
-	int r;
-	int w;
-	char *b;
+	int from;
+	int to;
+	int R;
+	int W;
+	char *buffer;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	b = create_buffer(argv[2]);
-	f = open(argv[1], O_RDONLY);
-	r = read(f, b, 1024);
-	t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	R = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (f == -1 || r == -1)
+		if (from == -1 || R == -1)
 		{
 			dprintf(STDERR_FILENO,
-					"Error: Cant't read from file %s\n", argv[1]);
-			free(b);
+					"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
 			exit(98);
 		}
 
-		w = write(t, b, r);
-		if (t == -1 || w == -1)
+		W = write(to, buffer, R);
+		if (to == -1 || W == -1)
 		{
 			dprintf(STDERR_FILENO,
 					"Error: Can't write to %s\n", argv[2]);
-			free(b);
+			free(buffer);
 			exit(99);
 		}
 
-		r = read(f, b, 1024);
-		t = open(argv[2], O_WRONLY | O_APPEND);
-	} while (r > 0);
+		R = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
 
-	free(b);
-	close_file(f);
-	close_file(t);
+	} while (R > 0);
+
+	free(buffer);
+	close_file(from);
+	close_file(to);
 	return (0);
 }
