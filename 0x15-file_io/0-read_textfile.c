a7fac4c4bd4541bcbfaa4c5e581f5172ef430e91
ynwvroy <bennetfrmdao@gmail.com>
2023-07-10 10:22:24 -0700

    Tread carefuly

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t w;
 	ssize_t t;
 
-	fd = open(filenme, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
